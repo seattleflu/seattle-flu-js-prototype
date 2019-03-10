d84e1571e2d65b6cbf72c6975cc8502a76a3c77a
jotasolano <jsolano91@gmail.com>
2019-03-10 16:36:44 -0400

    :rocket: feat(init): Initial commit with basic Mapbox implementation

--- README.md
+++ README.md
@@ -1,3 +1,20 @@
-# seattle-geojson-webpack
+# seattle-flu-js-prototype
 
-Describe seattle-geojson-webpack here.
+This repo contains prototypes and visualization sketches for the Web-based Seattle Flu platform.
+
+This project uses [NWB](https://github.com/insin/nwb) boilerplate technology. I have a general aversion to Webpack and Mapbox will not run using Parcel-Bundler, so this was a good alternative. If someone else can set up a stable Webpack environment, I'm happy to move the project to it.
+
+# How To Run
+
+1. Clone repo
+2. `npm install`
+3. To run locally: `npm start`
+
+
+# To Dos
+
+- [ ] Set up the general layout
+- [ ] Move the visualization from Observable to this layout
+- [ ] Refine map settings
+- [ ] Load all remaining maps
+- [ ] Test simple interactions
--- src/index.js
+++ src/index.js
@@ -1,3 +1,37 @@
+import "./main.scss";
+import * as d3 from 'd3';
+import mapboxgl from 'mapbox-gl/dist/mapbox-gl.js';
+import config from './config';
+
+import mapData from "./mapData/2016_seattle_cra.json";
+
+
+const mapboxToken = config.mapboxToken;
+mapboxgl.accessToken = mapboxToken;
+
 let app = document.querySelector('#app')
 
-app.innerHTML = '<h2>Welcome to seattle-geojson-webpack</h2>'
+
+const map = new mapboxgl.Map({
+  container: 'app',
+  style: 'mapbox://styles/mapbox/light-v9',
+  center: [-122.32, 47.60],
+  zoom: 10,
+});
+
+map.on("load", function() {
+  map.addSource(
+    'cra',
+    { type: 'geojson', data: mapData },
+    )
+
+  map.addLayer({
+    "id": "cra-join",
+    "type": "fill",
+    "source": "cra",
+    "paint": {
+      "fill-color": "#00aa99",
+      "fill-outline-color": "white"
+    }
+  }, 'waterway-label');
+})
