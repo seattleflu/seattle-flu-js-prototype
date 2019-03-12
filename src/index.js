95731dd8a9b8cc984d7931ce5a0ba836b69b3aec
jotasolano <jsolano91@gmail.com>
2019-03-11 22:01:14 -0400

    feat(maps): added all map layers and created simple hide/show toggle

--- src/index.js
+++ src/index.js
@@ -3,35 +3,125 @@ import * as d3 from 'd3';
 import mapboxgl from 'mapbox-gl/dist/mapbox-gl.js';
 import config from './config';
 
-import mapData from "./mapData/2016_seattle_cra.json";
+import mapData from "./mapData";
 
 
 const mapboxToken = config.mapboxToken;
 mapboxgl.accessToken = mapboxToken;
 
 let app = document.querySelector('#app')
+const menu = d3.select("#app").append("div").attr("id", "menu")
+const chartContainer = d3.select("#app").append("div").attr("id", "chart-container");
+const mapContainer = d3.select("#app").append("div").attr("id", "map-container");
 
 
 const map = new mapboxgl.Map({
-  container: 'app',
+  container: 'map-container',
   style: 'mapbox://styles/mapbox/light-v9',
   center: [-122.32, 47.60],
   zoom: 10,
 });
 
 map.on("load", function() {
-  map.addSource(
-    'cra',
-    { type: 'geojson', data: mapData },
-    )
-
+  map.addSource('city', {
+    type: 'geojson',
+    data: mapData.city
+  });
   map.addLayer({
-    "id": "cra-join",
+    "id": "city",
     "type": "fill",
-    "source": "cra",
+    "source": "city",
+    "layout": {
+      "visibility": "visible"
+    },
     "paint": {
       "fill-color": "#00aa99",
       "fill-outline-color": "white"
     }
-  }, 'waterway-label');
-})
+  });
+
+  map.addSource('neighborhoods', {
+    type: 'geojson',
+    data: mapData.neighborhoods
+  });
+  map.addLayer({
+    "id": "neighborhoods",
+    "type": "fill",
+    "source": "neighborhoods",
+    "layout": {
+      "visibility": "visible"
+    },
+    "paint": {
+      "fill-color": "#00aa99",
+      "fill-outline-color": "white"
+    }
+  });  
+
+  map.addSource('cra',{ 
+    type: 'geojson',
+    data: mapData.cra 
+  });
+  map.addLayer({
+    "id": "cra",
+    "type": "fill",
+    "source": "cra",
+    "layout": {
+      "visibility": "visible"
+    },
+    "paint": {
+      "fill-color": "#00aa99",
+      "fill-outline-color": "white"
+    }
+  });
+
+  map.addSource('censusTracts', {
+    type: 'geojson',
+    data: mapData.censusTracts
+  });
+  map.addLayer({
+    "id": "censusTracts",
+    "type": "fill",
+    "source": "censusTracts",
+    "layout": {
+      "visibility": "visible"
+    },
+    "paint": {
+      "fill-color": "#00aa99",
+      "fill-outline-color": "white"
+    }
+  });
+});
+
+
+const mapLevels = [...Object.keys(mapData)];
+
+for (let i = 0; i < mapLevels.length; i++) {
+  let id = mapLevels[i];
+  
+  let link = document.createElement('a');
+
+  link.href = '#';
+  link.className = 'active';
+  link.textContent = id;
+
+  link.onclick = function(e) {
+    console.log(e);
+    const clickedLayer = this.textContent;
+    e.preventDefault();
+    e.stopPropagation();
+
+    var visibility = map.getLayoutProperty(clickedLayer, 'visibility');
+
+    if (visibility === 'visible') {
+      map.setLayoutProperty(clickedLayer, 'visibility', 'none');
+      this.className = '';
+    } else {
+      this.className = 'active';
+      map.setLayoutProperty(clickedLayer, 'visibility', 'visible');
+    }
+  };
+
+  const layers = document.getElementById("menu");
+  layers.appendChild(link);
+  
+}
---
+++ src/mapData.js
@@ -0,0 +1,12 @@
+import city from "./mapData/2016_seattle_city.json";
+import neighborhoods from "./mapData/2016_seattle_neighborhoods.json";
+import cra from "./mapData/2016_seattle_cra.json";
+import censusTracts from "./mapData/2016_seattle_censusTracts.json";
+
+export default {
+  city,
+  neighborhoods,
+  cra,
+  censusTracts
+}
+
